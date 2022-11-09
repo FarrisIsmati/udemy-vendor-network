05dadf38cc5a28270508af84d8a222f64d181966
farris.ismati@gmail.com <farris.ismati@gmail.com>
2022-11-09 10:20:41 -0500

    fit

--- terraform/route_tables.tf
+++ terraform/route_tables.tf
@@ -2,11 +2,6 @@
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.aws-vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_internet_gateway.aws-igw.id
-  }
-
   tags = {
     Name                = "${var.app_name}-public-rt"
     udemy                = ""
@@ -15,11 +10,11 @@ resource "aws_route_table" "public" {
   }
 }
 
-# resource "aws_route" "public" {
-#   route_table_id         = aws_route_table.public.id
-#   destination_cidr_block = "0.0.0.0/0"
-#   gateway_id             = aws_internet_gateway.aws-igw.id
-# }
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.aws-igw.id
+}
 
 resource "aws_route_table_association" "public" {
   count          = length(var.public_subnets)
@@ -31,11 +26,6 @@ resource "aws_route_table_association" "public" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.aws-vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id     = aws_nat_gateway.public_subnet.id
-  }
-
   tags = {
     Name                = "${var.app_name}-private-rt"
     udemy                = ""
@@ -44,11 +34,11 @@ resource "aws_route_table" "private" {
   }
 }
 
-# resource "aws_route" "private" {
-#   route_table_id         = aws_route_table.private.id
-#   destination_cidr_block = "0.0.0.0/0"
-#   nat_gateway_id     = aws_nat_gateway.public_subnet.id
-# }
+resource "aws_route" "private" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id     = aws_nat_gateway.public_subnet.id
+}
 
 resource "aws_route_table_association" "private" {
   count          = length(var.private_subnets)
