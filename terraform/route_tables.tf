5dfc4d03a2c742ecbec4209a943175366ac951a5
farris.ismati@gmail.com <farris.ismati@gmail.com>
2022-11-10 09:45:20 -0500

    WHY NOT

--- terraform/route_tables.tf
+++ terraform/route_tables.tf
@@ -83,7 +83,7 @@ resource "aws_route_table" "private_rt_2" {
 #   route_table_id         = aws_route_table.private_rt_2.id
 #   destination_cidr_block = "0.0.0.0/0"
 #   nat_gateway_id     = aws_nat_gateway.public_subnet_2.id
-# }
+# } 
 
 resource "aws_route_table_association" "private_rt_2" {
   subnet_id      = aws_subnet.private_2.id
