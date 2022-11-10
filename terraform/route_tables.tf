a16b53c36cbf4fb5ef828c07d7f0a552d20e6f9a
farris.ismati@gmail.com <farris.ismati@gmail.com>
2022-11-10 09:09:37 -0500

    Fixies

--- terraform/route_tables.tf
+++ terraform/route_tables.tf
@@ -1,29 +1,51 @@
 # Public RT 1
-resource "aws_route_table" "public" {
+resource "aws_route_table" "public_rt_1" {
   vpc_id = aws_vpc.aws-vpc.id
 
   tags = {
-    Name                = "${var.app_name}-public-rt"
+    Name                = "${var.app_name}-public-1-rt"
     udemy                = ""
     vendor               = ""
     rt                   = ""
   }
 }
 
-resource "aws_route" "public" {
-  route_table_id         = aws_route_table.public.id
+resource "aws_route" "public_rt_1" {
+  route_table_id         = aws_route_table.public_rt_1.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.aws-igw.id
 }
 
-resource "aws_route_table_association" "public" {
-  count          = length(var.public_subnets)
-  subnet_id      = element(aws_subnet.public.*.id, count.index)
-  route_table_id = aws_route_table.public.id
+resource "aws_route_table_association" "public_rt_1" {
+  subnet_id      = aws_subnet.public_1
+  route_table_id = aws_route_table.public_rt_1
+}
+
+# Public RT 2
+resource "aws_route_table" "public_rt_2" {
+  vpc_id = aws_vpc.aws-vpc.id
+
+  tags = {
+    Name                = "${var.app_name}-public-2-rt"
+    udemy                = ""
+    vendor               = ""
+    rt                   = ""
+  }
+}
+
+resource "aws_route" "public_rt_2" {
+  route_table_id         = aws_route_table.public_rt_2.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.aws-igw.id
+}
+
+resource "aws_route_table_association" "public_rt_2" {
+  subnet_id      = aws_subnet.public_1
+  route_table_id = aws_route_table.public_rt_2
 }
 
 # Private RT 1
-resource "aws_route_table" "private_subnet_1" {
+resource "aws_route_table" "private_rt_1" {
   vpc_id = aws_vpc.aws-vpc.id
 
   tags = {
@@ -34,19 +56,19 @@ resource "aws_route_table" "private_subnet_1" {
   }
 }
 
-resource "aws_route" "private_subnet_1" {
-  route_table_id         = aws_route_table.private_subnet_1.id
+resource "aws_route" "private_rt_1" {
+  route_table_id         = aws_route_table.private_rt_1.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id     = aws_nat_gateway.public_subnet_1.id
 }
 
-resource "aws_route_table_association" "private_subnet_1" {
+resource "aws_route_table_association" "private_rt_1" {
   subnet_id      = aws_subnet.private_1.id
-  route_table_id = aws_route_table.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt_1.id
 }
 
 # Private RT 2
-resource "aws_route_table" "private_subnet_2" {
+resource "aws_route_table" "private_rt_2" {
   vpc_id = aws_vpc.aws-vpc.id
 
   tags = {
@@ -57,14 +79,14 @@ resource "aws_route_table" "private_subnet_2" {
   }
 }
 
-resource "aws_route" "private" {
-  route_table_id         = aws_route_table.private_subnet_2.id
+resource "aws_route" "private_rt_2" {
+  route_table_id         = aws_route_table.private_rt_2.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id     = aws_nat_gateway.public_subnet_2.id
 }
 
-resource "aws_route_table_association" "private_subnet_2" {
+resource "aws_route_table_association" "private_rt_2" {
   subnet_id      = aws_subnet.private_2.id
-  route_table_id = aws_route_table.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt_2.id
 }
 
