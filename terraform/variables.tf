f1cef5e19d0037245d9a8252785e190cb3a546e9
farris.ismati@gmail.com <farris.ismati@gmail.com>
2023-01-18 09:59:01 -0500

    updated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,11 @@
+# replace with github actions secret
 variable "aws_region" {
   type        = string
   description = "AWS Region"
   default     = "us-east-1"
 }
 
+# Will make this not a variable (don't add)
 variable "aws_cloudwatch_retention_in_days" {
   type        = number
   description = "AWS CloudWatch Logs Retention in Days"
@@ -16,27 +18,26 @@ variable "app_name" {
   default     = "udemy-vendor"
 }
 
+# Will not include (just used in tags)
 variable "app_environment" {
   type        = string
   description = "Application Environment"
   default     = "development"
 }
 
-variable "cidr" {
-  description = "The CIDR block for the VPC."
-  default     = "10.0.0.0/16"
-}
-
+# range of IP addresses we will use for public subnets (won't go into CIDR block ranges but you can read in description)
 variable "public_subnets" {
   description = "List of public subnets"
   default     = ["10.10.100.0/24", "10.10.101.0/24"]
 }
 
+# range of IP addresses we will use for private subnets (won't go into CIDR block ranges but you can read in description)
 variable "private_subnets" {
   description = "List of private subnets"
   default     = ["10.10.0.0/24", "10.10.1.0/24"]
 }
 
+# Physical zones AWS will put our resources in
 variable "availability_zones" {
   description = "List of availability zones"
   default     = ["us-east-1a", "us-east-1b"]
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,9 @@
 // Virtual private cloud (sets up our networks in AWS so we can communicate with the outside world) 
 resource "aws_vpc" "aws-vpc" {
   cidr_block            = "10.10.0.0/16" # Give a quick brief on what a CIDR block is and a resource related to it
+  # The Amazon DNS server is an Amazon Route 53 Resolver server. 
+  # This server enables DNS for instances that need to communicate over the VPC's internet gateway.
+  # To learn more https://docs.aws.amazon.com/vpc/latest/userguide/vpc-dns.html
   enable_dns_hostnames  = true
   enable_dns_support    = true
   tags = { # Redefine tag game (name and env only)
